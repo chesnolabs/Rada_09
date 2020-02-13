0c3e6d047b5de5ad8d6bbc74dd444964f20bc002
Okssana <40266071+Okssana@users.noreply.github.com>
2020-02-13 21:13:36 +0200

    Update amends_voting.R

--- amends_voting.R
+++ amends_voting.R
@@ -147,3 +147,94 @@ write.xlsx(as.data.frame(out_amends_mps_n),
 write.xlsx(as.data.frame(out_amends_mps_perc), 
            file=paste0("output_cumulative/amends_voting", ".xlsx"),
            sheetName="Нардепи за поправки у відсотках", row.names=FALSE, append = TRUE)
+
+
+
+#### Крок.9 Поправки, 225+1, пофракційно, % ####
+#### Крок. 9.1. Фільтруємо кількість голосів ####
+
+act_amends <- amendments_voting%>%
+  filter(for.>225)
+
+#### Крок. 9.2. Розшифровуємо персональне голосування ####
+out_act_amends <- cSplit(act_amends, "results", sep="|", "long")%>%
+  separate(results, c("mps_id", "faction", "vote_status"), ":")%>%
+  mutate(faction = recode(faction,
+                          `0` = "Позафракційні",
+                          `1` = "Слуга Народу",
+                          `2` = "ОПЗЖ",
+                          `3` = "Батьківщина",
+                          `4` = "ЄС",
+                          `5` = "ГОЛОС",
+                          `6` = "За майбутнє",
+                          `7`="ДОВІРА"))%>%
+  mutate(vote_status = recode(vote_status,
+                              `0` = "Відсутній",
+                              `1` = "За",
+                              `2` = "Проти",
+                              `3` = "Утримався",
+                              `4` = "Не голосував",
+                              `5` = "Присутній"))%>%
+  left_join(mps09, by=c("mps_id"="id_mp"))%>%
+  left_join(factions_09, by=c("mps_id"="rada_id"))%>%
+  mutate(id_question.x=as.character(id_question.x))
+#
+#### Крок. 9.3. По нардепам і фракціям ####
+act_amends_mps_n <- out_act_amends %>%
+  #mutate(mps_id=as.integer(mps_id))%>%
+  group_by(fullname, mps_id, factions)%>%
+  summarise(vote_for = sum(vote_status == "За"), 
+            vote_abstain = sum(vote_status == "Утримався"),
+            vote_against_ = sum(vote_status == "Проти"), 
+            vote_present = sum(vote_status == "Присутній"),
+            vote_not_voting = sum(vote_status == "Не голосував"),
+            vote_absent = sum(vote_status == "Відсутній")) %>% 
+  arrange(vote_for, fullname)%>%
+  left_join(mps_09, by=c("fullname"="full_name"))%>%
+  #left_join(factions_09, by=c("mps_id"="rada_id"))%>%
+  filter(date_end=="")%>%
+  select( -rada_id)
+
+#### Крок. 9.4. По нардепам і фракціям, %  ####
+act_amends_mps_perc <- out_act_amends %>%
+  group_by(fullname, mps_id, factions)%>%
+  summarise(
+    vote_for_perc = round(mean(vote_status == "За")*100, 0), 
+    vote_abstain_perc = round(mean(vote_status == "Утримався")*100, 0),
+    
+    vote_against_perc = round(mean(vote_status == "Проти")*100, 0), 
+    
+    vote_present_perc = round(mean(vote_status == "Присутній")*100, 0),
+    vote_not_voting_perc = round(mean(vote_status == "Не голосував")*100, 0),
+    vote_absent_perc = round(mean(vote_status == "Відсутній")*100, 0))%>%
+  arrange(vote_for_perc, fullname)%>%
+  left_join(mps_09, by=c("fullname"="full_name"))%>%
+  filter(date_end=="")%>%
+  select( -rada_id)
+
+
+#### Крок. 9.5. По фракціях ####
+
+out_act_amends_factions_perc <- out_act_amends%>%
+  left_join(mps_09, by=c("name"="full_name"))%>%  # ДФ з колонкою для філтрування date_end
+  filter(date_end=="")%>%                         # Прибираємо не депутатів
+  #filter(is.na(faction))%>%
+  #left_join(factions_09, by=c("mps_id"="rada_id"))%>%
+  group_by(factions)%>%
+  summarise(
+    # У відсотках
+    vote_for_perc = round(mean(vote_status == "За")*100, 1), 
+    vote_abstain_perc = round(mean(vote_status == "Утримався")*100, 1),
+    vote_against_perc = round(mean(vote_status == "Проти")*100, 1), 
+    vote_present_perc = round(mean(vote_status == "Присутній")*100, 1),
+    vote_not_voting_perc = round(mean(vote_status == "Не голосував")*100, 1),
+    vote_absent_perc = round(mean(vote_status == "Відсутній")*100, 1)) %>% 
+  arrange(vote_for_perc, factions)
+
+#### Крок. 9.6. По фракціях, long ####
+out_act_amends_factions_perc_long <- out_act_amends_factions_perc%>%
+  mutate(faction=as.factor(out_act_amends_factions_perc$factions))%>%
+  #mutate(faction = fct_reorder(faction, levels(out_act_amends_factions_perc$factions))) %>% 
+  gather(status, n_vote, vote_for_perc:vote_absent_perc, factor_key = TRUE)%>%
+  filter(!status=="vote_present_perc") # Фільтрує невідомий критерій з нулями.
+
